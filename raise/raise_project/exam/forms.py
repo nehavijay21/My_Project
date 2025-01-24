ca6657bc58c7856e012551509bf6863500c0548e
nehavijay21 <nehavijay8441@gmail.com>
2025-01-24 22:26:16 +0530

    error correct

--- raise/raise_project/exam/forms.py
+++ raise/raise_project/exam/forms.py
@@ -1,21 +1,24 @@
 from django import forms
-from .models import Programme  # Assuming Program is your model name
+from .models import Programme, Department  # Ensure these models exist
 
 class ProgramForm(forms.ModelForm):
-    LEVEL_CHOICES = [
-        ('U', 'Undergraduate'),
-        ('PG', 'Postgraduate'),
-        ('FYUG', 'Four Year Undergraduate'),
-        ('IPG', 'Integrated Postgraduate'),
-    ]
+    department = forms.ModelChoiceField(
+        queryset=Department.objects.all(),
+        empty_label="Select Department",
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
-    level = forms.ChoiceField(choices=LEVEL_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    level = forms.ChoiceField(
+        choices=[('UG', 'Undergraduate'), ('PG', 'Postgraduate'), 
+                 ('FYUG', 'Four Year UG'), ('IPG', 'Integrated PG')],
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = Programme
-        fields = ['programme_name', 'dept', 'level', 'duration']
+        fields = ['programme_name', 'department', 'level', 'duration']
         widgets = {
             'programme_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'dept': forms.Select(attrs={'class': 'form-control'}),
             'duration': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
--- raise/raise_project/exam/models.py
+++ raise/raise_project/exam/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     dept_id = models.AutoField(primary_key=True)
-    dept_name = models.CharField(max_length=100)
+    dept_name = models.CharField(max_length=100,unique=True)
 
     def __str__(self):
         return self.dept_name
 
 class Programme(models.Model):
-    LEVEL_CHOICES = [
-        ('UG', 'Undergraduate'),
-        ('PG', 'Postgraduate'),
-        ('FYUG','Four year UG'),
-        ('IPG','Integrated PG'),
-    ]
+    # LEVEL_CHOICES = [
+    #     ('UG', 'Undergraduate'),
+    #     ('PG', 'Postgraduate'),
+    #     ('FYUG','Four year UG'),
+    #     ('IPG','Integrated PG'),
+    # ]
     programme_name = models.CharField(max_length=255)
-    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
-    level = models.CharField(max_length=20, choices=LEVEL_CHOICES)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    level = models.CharField(max_length=20, choices=[('UG','UnderGraduate'),('PG','PostGraduate'),('FYUG','Four Year UG'),('IPG','Integrated PG')])
     duration = models.IntegerField()
 
     def __str__(self):
